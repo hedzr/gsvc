f05207bc21721c3ad4ef15fedbf97cd44cdbe57d
hz <hedzrz@gmail.com>
2025-04-17 08:31:03 +0800

    fix top pkg name

--- doc.go
+++ doc.go
@@ -1,1 +1,5 @@
-package is
+package gsvc
+
+const appName = "gsvc"   // appName of hedzr/is package
+const version = "v0.1.0" // version of hedzr/is package | update it while bumping hedzr/is' version
+const Version = version
