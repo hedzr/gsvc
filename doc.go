7f1038594acd91fb748bc554d01c37387df3f096
hz <hedzrz@gmail.com>
2025-04-17 12:03:18 +0800

    bump to v0.3.1

--- doc.go
+++ doc.go
@@ -1,5 +1,5 @@
 package gsvc
 
 const appName = "gsvc"   // appName of hedzr/is package
-const version = "v0.3.0" // version of hedzr/is package | update it while bumping hedzr/is' version
+const version = "v0.3.1" // version of hedzr/is package | update it while bumping hedzr/is' version
 const Version = version
